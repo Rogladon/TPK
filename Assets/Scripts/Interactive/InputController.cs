33e908325e96ce8b2254934cfe19550dd2076c38
Александр Бахаревский <bakharevskiya@mail.ru>
2022-02-25 18:49:37 +0300

    wip: смена курсора

--- Assets/Scripts/Interactive/InputController.cs
+++ Assets/Scripts/Interactive/InputController.cs
@@ -5,8 +5,24 @@ using UnityEngine;
 
 namespace Assets.Scripts {
 	public class InputController : MonoBehaviour {
+		[System.Serializable]
+		private class CursorTexture {
+			public static CursorTexture current;
 
+			public Texture2D texture;
+			public Vector2 hotspot;
+			public CursorMode cursorMode;
+
+			public void Apply() {
+				if (current == this) return;
+				current = this;
+				Cursor.SetCursor(texture, hotspot, cursorMode);
+			}
+		}
 		#region Fields
+		[SerializeField] private CursorTexture defaultCursor;
+		[SerializeField] private CursorTexture selectCursor;
+
 		private IInteractable currentInteractable = null;
 		#endregion
 
@@ -25,6 +41,7 @@ namespace Assets.Scripts {
 			if(Physics.Raycast(ray, out hit)) {
 				IInteractable interactable = null;
 				if(hit.transform.TryGetComponent(out interactable)) {
+					selectCursor.Apply();
 					if(currentInteractable != null && interactable != currentInteractable) {
 						interactable.OnPointerEnter();
 						currentInteractable.OnPointerExit();
@@ -32,6 +49,7 @@ namespace Assets.Scripts {
 					if (Input.GetMouseButtonDown(0)) interactable.OnPointerDown();
 					currentInteractable = interactable;
 				} else {
+					defaultCursor.Apply();
 					if (currentInteractable != null) currentInteractable.OnPointerExit();
 				}
 				if (Input.GetMouseButtonUp(0)) {
@@ -39,6 +57,8 @@ namespace Assets.Scripts {
 				}
 			}
 		}
+
+		
 		#endregion
 	}
 }
