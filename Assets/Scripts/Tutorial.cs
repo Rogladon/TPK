70aaf92bb077d43167c9b1859ecc2f4c599f5f8c
Александр Бахаревский <bakharevskiya@mail.ru>
2022-02-25 18:50:16 +0300

    wip: механизм надписей интерактивнх объектов

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Tutorial : MonoBehaviour {
+	#region Fields
+
+	#endregion
+
+	#region Properties
+
+	#endregion
+
+	#region Public Methods
+
+	#endregion
+
+	#region Private Methods
+
+	#endregion
+}
